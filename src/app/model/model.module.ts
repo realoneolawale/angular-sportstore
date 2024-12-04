174f33f550aa295c369a142301db5f58918f1043
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-12-04 07:10:31 +0100

    First commit

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
+import { RestDataSource } from "./rest.datasource";
+import { HttpClientModule } from "@angular/common/http";
 // featured module - this is added to the root module 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart]
+    imports: [HttpClientModule],
+    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository, RestDataSource, { provide: StaticDataSource, useClass: RestDataSource}]
 })
 export class ModelModule { }
--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -12,7 +12,9 @@ export class Order {
     public zip?: string;
     public country?: string;
     public shipped: boolean = false;
+
     constructor(public cart: Cart) { }
+
     clear() {
         this.id = undefined;
         this.name = this.address = this.city = undefined;
